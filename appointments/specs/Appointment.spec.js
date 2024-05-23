152d7206cae6a77ecc944a1d4344423ced11adf8
Michael Micek <mmdev@micek.csz.com>
2024-05-23 14:54:55 -0700

    R2 remove duplication in spec. p26
    Co-authored-by: Alan Wostenberg <awostenberg@gmail.com>

--- appointments/specs/Appointment.spec.js
+++ appointments/specs/Appointment.spec.js
@@ -1,33 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {act} from "react";
+import { act } from "react";
 
 import { Appointment } from "../src/Appointment.js";
 
 describe("Appointment", () => {
-    it("renders customer first name", () => {
-        const customer = { firstName: "Ashley" };
-        const component = <Appointment customer={customer} />;
-        const container = document.createElement("div");
+    let container;
+    beforeEach(() => {
+        container = document.createElement("div");
         document.body.replaceChildren(container);
+    })
 
-        act( () => 
+    const render = component =>
+        act(() =>
             ReactDOM.createRoot(container).render(component)
         );
-        
+
+    it("renders customer first name", () => {
+        const customer = { firstName: "Ashley" };
+        render(<Appointment customer={customer} />);
         expect(document.body.textContent).toContain("Ashley");
     })
 
     it("renders another customer first name", () => {
         const customer = { firstName: "Jordan" };
-        const component = <Appointment customer={customer} />;
-        const container = document.createElement("div");
-        document.body.replaceChildren(container);
-
-        act( () => 
-            ReactDOM.createRoot(container).render(component)
-        );
-        
+        render(<Appointment customer={customer} />);
         expect(document.body.textContent).toContain("Jordan");
     })
- })
+})
--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -1,2 +1,7 @@
 import React from "react";
-export const Appointment = ({ customer }) => customer.firstName;
+export const Appointment = ({ customer }) => (
+    <div>{customer.firstName}</div>
+);  // Note: also passed without <div>{}</div>
+    // Note: do not understand how import Readt activates html syntax
+
+// npm test -- --watchAll
