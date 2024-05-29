60c75988409847ba75d0c593e5ea815841459a3d
Michael Micek <mmdev@micek.csz.com>
2024-05-29 16:00:02 -0700

    F2!! Ch2 p42 render detailed list
    Co-authored-by: Alan Wostenberg <awostenberg@gmail.com>

--- appointments/specs/Appointment.spec.jsx
+++ appointments/specs/Appointment.spec.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { act } from "react";
 
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
+
+
 
 describe("Appointment", () => {
     let container;
@@ -27,4 +29,69 @@ describe("Appointment", () => {
         render(<Appointment customer={customer} />);
         expect(document.body.textContent).toContain("Jordan");
     })
+})
+describe("AppointmentsDayView", () => {
+
+    const today = new Date();   //coupling
+    const twoAppointments = [
+        { startsAt: today.setHours(12, 0) },
+        { startsAt: today.setHours(13, 0) },
+    ];
+
+    let container;
+
+    beforeEach(() => {
+        container = document.createElement("div");
+        document.body.replaceChildren(container);
+
+    })
+    const render = (component) =>
+        act(() =>
+            ReactDOM.createRoot(container).render(component)    //todo duplication other render
+        );
+
+    it("renders a div with the right id", () => {
+        render(<AppointmentsDayView appointments={[]} />);
+        expect(
+            document.querySelector(
+                "div#appointmentsDayView"
+            )
+        ).not.toBeNull();
+    });
+
+    it("renders an OL element to display apointments", () => {
+        render(<AppointmentsDayView appointments={[]}/>);
+
+        const listElement = document.querySelector("ol");
+
+        expect(listElement).not.toBeNull();
+
+    });
+
+    it("renders an li for each appointment", () => {
+
+        render(
+            <AppointmentsDayView
+            appointments={twoAppointments}
+            />
+        );
+
+        const listChildren =
+            document.querySelectorAll("li");    //note did not need "ol > li"
+        expect(listChildren).toHaveLength(2);
+    });
+
+    it("renders the time for each appointment", () => {
+
+        render(
+            <AppointmentsDayView
+            appointments={twoAppointments}
+            />
+        );
+
+        const listChildren =
+            document.querySelectorAll("li");
+        expect(listChildren[0].textContent).toEqual("12:00");
+        expect(listChildren[1].textContent).toEqual("13:00");
+    });
 })
--- appointments/src/Appointment.jsx
+++ appointments/src/Appointment.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+
+const appointmentTimeOfDay = (startsAt) => {
+    const [h, m] = new Date(startsAt)
+        .toTimeString()
+        .split(":");
+    return `${h}:${m}`;
+}
+
 export const Appointment = ({ customer }) => (
     <div>{customer.firstName}</div>
 );  // Note: also passed without <div>{}</div>
     // Note: do not understand how import Readt activates html syntax
 
+
+export const AppointmentsDayView = (
+    { appointments }
+) => (
+    <div id="appointmentsDayView">
+        <ol>
+            {appointments.map(appointment => (
+                <li key={appointment.startsAt}>
+                    {appointmentTimeOfDay(appointment.startsAt)}
+                </li>
+            ))}
+        </ol>
+    </div>
+);
 // npm test -- --watchAll
